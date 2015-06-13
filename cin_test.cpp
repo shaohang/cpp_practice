8e0f387fdd19488ec08706f60f82fb79b56e2335
shaohang <shao.hang523@gmail.com>
2015-06-12 23:32:00 -0400

    add cin test

---
+++ cin_test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout<< "How many dog do you have?\n";
+	cin >> a;
+
+	cout<< "You have " << a << " dogs.\n";
+
+	return 0;
+}
